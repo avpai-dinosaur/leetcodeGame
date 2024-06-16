04ae566b5888c595006c6f50ab1b0abbec8ec809
Dakshesh Daruri <dakshesh@Daksheshs-MacBook-Pro-3.local>
2024-06-16 17:30:10 -0400

    new stuff with animation

--- main.py
+++ main.py
@@ -12,52 +12,17 @@ def main():
     running = True
 
     #TEMPORARY
-    image = pygame.image.load('Oldhero.png').convert_alpha()
+
     color = (0,0,0)
-    sheet = spritesheet.SpriteSheet(image)
-    animationrun = []
+    sheet = spritesheet.SpriteSheet('Oldhero.png', color)
     animationjump = []
     animationpunch = []
     animationkick = []
-    animationidle = []
-    masteraction = {"idle": 2, "run" : 6 , "jump" : 4, "punch" : 3, "kick" : 4}
+
     last_update = pygame.time.get_ticks()
     animation_cooldown = 200 #in milli secs
     frame = 0
-
-    #IDLE
-    #MAKE IDLE SLOWER TIME
-    for x in range(masteraction["idle"]):
-        animationidle.append(sheet.get_image(0, 0, 16 ,16, 3, color))
-        animationidle.append(sheet.get_image(1, 0, 16,16, 3, color))
-
-
-    for x in range(masteraction["run"]):
-        animationrun.append(sheet.get_image(0, 1, 16,16, 3, color))
-        animationrun.append(sheet.get_image(1, 1, 16,16, 3, color))
-        animationrun.append(sheet.get_image(2, 1, 16,16, 3, color))
-        animationrun.append(sheet.get_image(3, 1, 16,16, 3, color))
-        animationrun.append(sheet.get_image(4, 1, 16,16, 3, color))
-        animationrun.append(sheet.get_image(5, 1, 16,16, 3, color))
-    
-    for x in range(masteraction["jump"]):
-        animationjump.append(sheet.get_image(1, 0, 16,16, 3, color))
-        animationjump.append(sheet.get_image(2, 0, 16,16, 3, color))
-        animationjump.append(sheet.get_image(3, 0, 16,16, 3, color))
-        animationjump.append(sheet.get_image(4, 0, 16,16, 3, color))
-    
-    for x in range(masteraction["punch"]):
-        animationpunch.append(sheet.get_image(0, 4, 16,16, 3, color))
-        animationpunch.append(sheet.get_image(1, 4, 16,16, 3, color))
-        animationpunch.append(sheet.get_image(2, 4, 16,16, 3, color))
-
-    for x in range(masteraction["kick"]):
-        animationkick.append(sheet.get_image(0, 2, 16,16, 3, color))
-        animationkick.append(sheet.get_image(1, 2, 16,16, 3, color))
-        animationkick.append(sheet.get_image(2, 2, 16,16, 3, color))
-        animationkick.append(sheet.get_image(3, 2, 16,16, 3, color))
-
-    
+ 
     # Game loop
     while running:
         # Poll for events.
@@ -73,7 +38,7 @@ def main():
         world.update()
         world.draw(screen)
 
-        action = "idle"
+        action = "run"
         #current time
         current_time = pygame.time.get_ticks()
         if(current_time - last_update >= animation_cooldown):
@@ -81,9 +46,9 @@ def main():
             frame += 1
             last_update = current_time
             #reset frame back to 0 so it doesn't index out of bounds
-            if(frame >= masteraction[action]):
+            if(frame >= sheet.masteraction[action]):
                 frame = 0
-        screen.blit(animationidle[frame], (0,0))
+        screen.blit(sheet.animationrun[frame], (100,100))
 
 
 
