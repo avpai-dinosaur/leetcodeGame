51cefa12d7be289658971c63e74e149aeec5247c
Dakshesh Daruri <dakshesh@Daksheshs-MacBook-Pro-3.local>
2024-07-12 13:20:26 -0400

    menu stuff

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 import pygame
+import sys
 from world import World
-import requests as re
+from button import Button
+
+def menu():
+    screen = pygame.display.set_mode((1280, 800))
+    pygame.display.set_caption("Menu")
+    font=pygame.font.SysFont("cambria", 40)
+    play_img = pygame.image.load("data/images/Play.png")
+    background = pygame.image.load("data/images/menu_background.png")
+    while True: 
+        screen.blit(background,(0,0))
+        menu_mouse_pos = pygame.mouse.get_pos()
+        menu_text = font.render("MAIN MENU", True, "#b68f40")
+        menu_rect = menu_text.get_rect(center = (640, 100))
+
+        play_button = Button(play_img, pos=(640, 400), 
+                            text_input="PLAY", font=pygame.font.SysFont("cambria", 40), base_color="#d7fcd4", hovering_color="White")
+  
+
+        screen.blit(menu_text, menu_rect)
+        buttons = [play_button]
+        for button in buttons:
+            button.changeColor(menu_mouse_pos)
+            button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.checkForInput(menu_mouse_pos):
+                    main()
+              
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((1280, 800))
+    
     clock = pygame.time.Clock()
     world = World()
     running = True
- 
+    #menu stuff
+    
     # Game loop
     while running:
         # Poll for events.
@@ -21,6 +53,7 @@ def main():
         
         # fill the screen with a color to wipe away anything from last frame
         screen.fill("black")
+
         world.update()
         world.draw(screen)
 
@@ -32,4 +65,6 @@ def main():
     pygame.quit()
 
 if __name__ == "__main__":
-    main()
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 800))
+    menu()
