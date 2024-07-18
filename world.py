bc5bb66daed483d4e6c54014f553c5beb56a4b5e
Ashvin Pai <avpai@umich.edu>
2024-07-18 00:53:27 -0400

    draw only sprites on to screen if they are within game window

--- world.py
+++ world.py
@@ -20,14 +20,16 @@ class Camera(pygame.sprite.Group):
         self.center_camera_on_target(init_pos)
 
         # Zoom
-        self.zoom = 2
-        self.internal_surface_size = (surface.get_size()[0], surface.get_size()[1]) # tune to what looks good w/o excessively slowing game
+        self.zoom = 1
+        self.internal_surface_size = (surface.get_size()[0] * 2, surface.get_size()[1] * 2) # tune to what looks good w/o excessively slowing game
         self.internal_surface = pygame.Surface(self.internal_surface_size, pygame.SRCALPHA)
         self.internal_rect = self.internal_surface.get_rect(center=(self.half_w, self.half_h))
         self.internal_surface_size_vector = pygame.math.Vector2(self.internal_surface_size)
         self.internal_offset = pygame.math.Vector2()
         self.internal_offset.x = self.internal_surface_size[0] // 2 - self.half_w
         self.internal_offset.y = self.internal_surface_size[1] // 2 - self.half_h
+        self.x_bound_distance = self.half_w
+        self.y_bound_distance = self.half_h
     
     def center_camera_on_target(self, target):
         """Centers the camera on the target rect.
@@ -40,24 +42,39 @@ class Camera(pygame.sprite.Group):
     def zoom_keyboard_control(self):
         """Control the zoom level with keyboard."""
         keys = pygame.key.get_just_pressed()
+        pressed_key = False
         if keys[pygame.K_q]:
+            pressed_key = True
             if self.zoom < 2.5:
                 self.zoom += 0.5 
         if keys[pygame.K_e]:
-            if self.zoom > 1:
+            pressed_key = True
+            if self.zoom > 0.5:
                 self.zoom -= 0.5
+        if pressed_key:
+            self.x_bound_distance = self.half_w / self.zoom
+            self.y_bound_distance = self.half_h / self.zoom
 
     def update(self, player_rect):
         """Update the camera."""
         self.zoom_keyboard_control()
         self.center_camera_on_target(player_rect)
 
-    def draw(self, surface):
+    def draw_filter(self, player_rect, sprite_rect):
+        """Filter out sprites outside of game window."""
+        if abs(player_rect.centerx - sprite_rect.centerx) > self.x_bound_distance:
+            return False
+        if abs(player_rect.centery - sprite_rect.centery) > self.y_bound_distance:
+            return False
+        return True
+
+    def draw(self, player_rect, surface):
         """Draw the sprites belonging to the camera group to surface."""
         self.internal_surface.fill((0, 0, 0))
         self.internal_surface.blit(self.background, -self.offset + self.internal_offset)
         for sprite in sorted(self.sprites(), key=lambda s : s.rect.centery):
-            sprite.draw(self.internal_surface, -self.offset + self.internal_offset)
+            if self.draw_filter(player_rect, sprite.rect):
+                sprite.draw(self.internal_surface, -self.offset + self.internal_offset)
         
         scaled_surface = pygame.transform.scale(self.internal_surface, self.zoom * self.internal_surface_size_vector)
         scaled_rect = scaled_surface.get_rect(center=(self.half_w, self.half_h))
@@ -88,7 +105,7 @@ class World():
         # self.map.antidote_doors.update(self.player)
     
     def draw(self, surface):
-        self.camera.draw(surface)
+        self.camera.draw(self.player.rect, surface)
         #self.player.draw(surface)
         # for enemy in self.enemies:
         #     enemy.draw(surface)
